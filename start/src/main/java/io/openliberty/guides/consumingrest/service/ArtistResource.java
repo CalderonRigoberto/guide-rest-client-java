ec4f9b4c45f6441d926301215304eade3b132f30
Rigoberto Calderon <rcalderon0817@gmail.com>
2022-08-03 17:40:25 -0500

    Consumir los recursos en Java usando JSONB y JSONP.

--- start/src/main/java/io/openliberty/guides/consumingrest/service/ArtistResource.java
+++ start/src/main/java/io/openliberty/guides/consumingrest/service/ArtistResource.java
@@ -1,15 +1,3 @@
-// tag::copyright[]
-/*******************************************************************************
- * Copyright (c) 2017, 2022 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     IBM Corporation - Initial implementation
- *******************************************************************************/
-// end::copyright[]
 package io.openliberty.guides.consumingrest.service;
 
 import jakarta.json.JsonArray;
@@ -24,7 +12,8 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.UriInfo;
 
 import io.openliberty.guides.consumingrest.model.Artist;
-//import io.openliberty.guides.consumingrest.Consumer;
+import io.openliberty.guides.consumingrest.Consumer;
+
 
 @Path("artists")
 public class ArtistResource {
@@ -38,6 +27,39 @@ public class ArtistResource {
       return Reader.getArtists();
     }
 
-    //Add additional methods here
+    @GET
+    @Path("jsonString")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getJsonString() {
+      Jsonb jsonb = JsonbBuilder.create();
+     
+      Artist[] artists = Consumer.consumeWithJsonb(uriInfo.getBaseUri().toString()
+                                                   + "artists");
+      String result = jsonb.toJson(artists);
 
+      return result;
+    }
+
+    @GET
+    @Path("total/{artist}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public int getTotalAlbums(@PathParam("artist") String artist) {
+      Artist[] artists = Consumer.consumeWithJsonb(uriInfo.getBaseUri().toString()
+        + "artists");
+
+      for (int i = 0; i < artists.length; i++) {
+        if (artists[i].name.equals(artist)) {
+          return artists[i].albums.length;
+        }
+      }
+      return -1;
+    }
+
+    @GET
+    @Path("total")
+    @Produces(MediaType.TEXT_PLAIN)
+    public int getTotalArtists() {
+      return Consumer.consumeWithJsonp(uriInfo.getBaseUri().toString()
+                                       + "artists").length;
+    }
 }
